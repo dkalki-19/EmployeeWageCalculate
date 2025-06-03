14aef7a573878ba9f03fb0930f6042eaf5d5e30e
dkalki-19 <dkalkicdhandapani@gmail.com>
2025-06-03 17:13:57 +0530

    updating main.java file

--- src/employeeWage/Main.java
+++ src/employeeWage/Main.java
@@ -1,5 +1,23 @@
 package employeeWage;
 
 public class Main {
+	public static void main(String[] args) {
+		System.out.println("Welcome to Employee Wage Computation Program ");
+        EmployeeWageComputation empWage = new EmployeeWageComputation();
 
+        empWage.checkAttendance();
+        
+        empWage.calculateDailyWage();
+
+        empWage.calculatePartTimeWage();
+
+        empWage.calculateUsingSwitch();
+
+        empWage.calculateMonthlyWage();
+
+        empWage.calculateTotalWageWithCondition();
+
+        empWage.computeEmployeeWage();
+	}
+	
 }
